da2565979af7355bcb6205122bcc62b496790e6a
Daniel Ricardo ZUluaga Lezcano <50756227+rikuso@users.noreply.github.com>
2025-03-12 18:18:56 -0500

    Update api.service.ts
    actualizaicon

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -7,9 +7,9 @@ import { Observable } from 'rxjs';
 })
 export class ApiService {
   
-  //private apiUrl = 'http://localhost:3000/api'; // Cambia esto a la URL de tu backend
 
-  private apiUrl = 'http://backend-qtap.vercel.app/api'
+
+  private apiUrl = 'http://backend-qtap.vercel.app/api';
   
   constructor(private http: HttpClient) { }
 
